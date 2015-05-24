59438996e9ac59b3b3dbcf427d9e4931a836520f
anetegithub <anete.anetes@gmail.com>
2015-05-24 22:38:11 +0500

    Campagin process = Yes

--- Hexagonia2015/Hexagonia2015/www/scripts/campagin.js
+++ Hexagonia2015/Hexagonia2015/www/scripts/campagin.js
@@ -147,69 +147,98 @@
         CampaginTime = true;
         this.init();
     },
+    _inited:false,
     init: function () {
-        dcanvaselement.addEventListener('click', function () {
-            if (CampaginTime) {
-                var Coords = ui.map.getAbstractHex(ui.mousePos); // ui.map.positionByBlockType(BLOCK.BASE),
-                if (Coords != undefined) {
-                    //Coords have x and y
-                    var Area = Enumerable
-                        .From(campw.location)
-                        .Where(function (x) { if (x.X == Coords.x && x.Y == Coords.y) return x; })
-                        .Select(function (x) { return x; })
-                        .ToArray();
+        if (!this._inited) {
+            this._inited = true;
+            dcanvaselement.addEventListener('click', function () {
+                if (CampaginTime) {
+                    var Coords = ui.map.getAbstractHex(ui.mousePos); // ui.map.positionByBlockType(BLOCK.BASE),
+                    if (Coords != undefined) {
+                        //Coords have x and y
+                        var Area = Enumerable
+                            .From(campw.location)
+                            .Where(function (x) { if (x.X == Coords.x && x.Y == Coords.y) return x; })
+                            .Select(function (x) { return x; })
+                            .ToArray();
 
-                    if (Area.length > 0) {
-                        var size = ($('#display-wrapper').width() - 5) / 7;
-                        if (Area[0].Base.State == Process.End) {
-                            var data={
-                                IsGold:false,
-                                IsCrystal:false,
-                                IsBlock: true,
-                                Data:{
-                                    TileName: "tileGrass",
-                                    Layer: 0,
-                                    Source: "images/buildings/ground.json",
-                                    SourceY: 0, //number of plus by Y coord, cuz merged tiles have Y shift
-                                    Land: 0,
-                                    Cost: 50,
-                                    Currency: 0,
-                                    State: 0
-                                }
-                            };
-                            data.IsBlock = false;
-                            data.IsGold = false;
-                            data.IsCrystal = true;
-                            data.Data = 50;
+                        if (Area.length > 0) {
+                            var size = ($('#display-wrapper').width() - 5) / 7;
+                            if (Area[0].Base.State == Process.End) {
+                                var data = {
+                                    IsGold: false,
+                                    IsCrystal: false,
+                                    IsBlock: true,
+                                    Data: {
+                                        TileName: "tileGrass",
+                                        Layer: 0,
+                                        Source: "images/buildings/ground.json",
+                                        SourceY: 0, //number of plus by Y coord, cuz merged tiles have Y shift
+                                        Land: 0,
+                                        Cost: 50,
+                                        Currency: 0,
+                                        State: 0
+                                    }
+                                };
+                                data.IsBlock = false;
+                                data.IsGold = false;
+                                data.IsCrystal = true;
+                                data.Data = 50;
 
-                            campw.reward_data = data;
+                                campw.reward_data = data;
 
-                            loading.show();
-                            alert("ajax to server");//new player object, info about gold/cryst/block
-                            loading.hide();
-                            var box = bootbox.dialog({
-                                closeButton: false,
-                                title: "<div class='text-center'>Reward</label><script>$('.modal-footer').html(\"<div class='text-center'><button class='btn btn-success form-control'>Ok</button></div>\");",
-                                message: "<div class='text-center'><canvas id='rewardcanvas' width='" + size.toString() + "' height='" + size.toString() + "'></canvas><script>campw.reward();</script></div>",
-                                buttons: { success: { label: "Ok", className: "btn-success", callback: function () { } } }
-                            });
-                            box.css({
-                                'top': '50%',
-                                'margin-top': function () {
-                                    return -(box.height() / 2);
-                                }
-                            });
-                        } else if (Area[0].Base.State == Process.Yes) {
-                            //$('.progress-bar').css('width', valeur + '%').attr('aria-valuenow', valeur);
-                            var bar = "<div class='progress'><div class='progress-bar' role='progressbar' aria-valuenow='75' aria-valuemin='0' aria-valuemax='100' style='width:75%;color:black;background-color:#ffd800'></div></div>";
+                                loading.show();
+                                alert("ajax to server");//new player object, info about gold/cryst/block
+                                loading.hide();
+                                var box = bootbox.dialog({
+                                    closeButton: false,
+                                    title: "<div class='text-center'>Reward</label><script>$('.modal-footer').html(\"<div class='text-center'><button class='btn btn-success form-control'>Ok</button></div>\");",
+                                    message: "<div class='text-center'><canvas id='rewardcanvas' width='" + size.toString() + "' height='" + size.toString() + "'></canvas><script>campw.reward();</script></div>",
+                                    buttons: { success: { label: "Ok", className: "btn-success", callback: function () { } } }
+                                });
+                                box.css({
+                                    'top': '50%',
+                                    'margin-top': function () {
+                                        return -(box.height() / 2);
+                                    }
+                                });
+                            } else if (Area[0].Base.State == Process.Yes) {
+                                var data = {
+                                    Now: 0,
+                                    Day: 24,
+                                    Month: 5,
+                                    Year: 2015,
+                                    Hour: 22,
+                                    Minutes: 40,
+                                    Seconds: 0,
+                                    Total:240
+                                };
+                                var myDate = new Date(data.Year, data.Month, data.Day, data.Hour, data.Minutes, data.Seconds);                                
+                                var bar = "<div class='progress'><div id='progress-bar-expedition' class='progress-bar' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100' style='width:0%;color:black;background-color:#ffd800'></div></div>";
+                                var box = bootbox.dialog({
+                                    closeButton: false,
+                                    title: "<div class='text-center'>Expedition</label><script>$('.modal-footer').html(\"<div class='text-center'><button class='btn btn-success form-control'>Ok</button></div>\");",
+                                    message: "<div class='row'><div class='col-md-6'>" + bar + "</div></div><div class='row'><div class='col-md-6'><div id='countdown' class='text-center'></div></div>",
+                                    buttons: { success: { label: "Ok", className: "btn-success", callback: function () { } } }
+                                });
+                                box.css({
+                                    'top': '50%',
+                                    'margin-top': function () {
+                                        return -(box.height() / 2);
+                                    }
+                                });
+                                timer.totalseconds = data.Total;
+                                timer.enable(myDate);                                
+                            }
                         }
-                    }
 
-                    ui.map.draw(campw.location);
+                        ui.map.draw(campw.location);
+                    }
                 }
-            }
-        });
+            });
+        }
     },
+    now_data:null,
     reward_data:null,
     reward: function(){
         var img = new Image(),
---
+++ Hexagonia2015/Hexagonia2015/www/scripts/timer.js
@@ -0,0 +1,52 @@
+﻿// set the date we're counting down to
+var target_date = new Date('Oct, 9, 2014').getTime();
+
+var second=0;
+
+// variables for time units
+var days, hours, minutes, seconds, ms_step = 60;
+
+var timer = {
+    totalseconds: 0,
+    godwhy:0,
+    _timerId: 0,
+    sad: 0,
+    suka:0,
+    enable: function (date) {
+        target_date = date;        
+        this._timerId = setInterval(function () {            
+            var current_date = new Date().getTime();
+            var seconds_left = (target_date - current_date) / 1000;
+            days = parseInt(seconds_left / 86400);
+            seconds_left = seconds_left % 86400;
+            hours = parseInt(seconds_left / 3600);
+            seconds_left = seconds_left % 3600;
+            min = parseInt(seconds_left / 60);
+            sec = parseInt(seconds_left % 60);
+            ms = parseInt(target_date - current_date);
+
+            $('#countdown').html('' +
+               '<span class="hours">' + (hours >= 10 ? hours.toString() : "0" + hours.toString()) + ':</span>' +
+               '<span class="minutes">' + (min >= 10 ? min.toString() : "0" + min.toString()) + ':</span>' +
+               '<span class="seconds">' + (sec >= 10 ? sec.toString() : "0" + sec.toString()) + '</span>');
+            
+            timer.godwhy = timer.totalseconds - seconds_left;
+            var percent = timer.godwhy / timer.totalseconds;
+            percent *= 100;
+            $(".progress-bar").css("width", percent.toString() + "%").attr("aria-valuenow", percent.sad);
+                            //$('#progress-bar-expedition').css('width', timer.sad + '%');
+                timer.sad++;
+                //$('#progress-bar-expedition').html($('#progress-bar-expedition').css('width'));            
+        }, ms_step);
+    },
+    disable: function () {
+        clearInterval(this._timerId);
+    }
+};
+
+var _DateTimeSimpleObject = {
+    Day: 1,
+    Month: 1,
+    Year: 1993
+};
+
